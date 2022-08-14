0e9c91e601942bf98a8e23ab3fe68a4c9cbc7265
Shabiul <86909796+Shabiul@users.noreply.github.com>
2022-08-14 09:59:03 +0530

    Add files via upload

---
+++ 01_loop/BinToDeci.java
@@ -0,0 +1,40 @@
+
+
+//WAP in java to accept a binary number and convert to decimal equivalent
+
+import java.util.*;
+class BinToDeci
+{
+    long bin, deci;
+    BinToDeci()
+    {
+        bin=deci=0;
+    }
+    public void input()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        bin = sc.nextLong();
+    }
+    public void compute()
+    {
+        long div,digit,p=0;
+        for(div=bin;div>0;div=div/10)
+        {
+            digit = div%10;
+            deci += digit * (long)Math.pow(2,p);
+            p++;
+        }
+    }
+    public void display()
+    {
+        System.out.println("Decimal is: " + deci);
+    }
+    public static void main(String args[])
+    {
+        BinToDeci ob = new BinToDeci();
+        ob.input();
+        ob.compute();
+        ob.display();
+    }
+}
---
+++ 01_loop/DeciToBin.java
@@ -0,0 +1,49 @@
+
+/*
+WAP in java to accept a decimal number and convert to binary equivalent.
+(assume that the binary equivalent does not end with zeros)
+
+*/
+import java.util.*;
+class DeciToBin
+{
+    long deci, bin;
+    DeciToBin()
+    {
+        deci=bin=0;
+    }
+    public void input()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        deci = sc.nextLong();
+    }
+    public void compute()
+    {
+        long div,digit,rems=0;
+        for(div=deci;div>0;div=div/2)
+        {
+            digit = div%2;
+            rems = rems * 10 + digit;
+        }
+        
+        bin = 0;
+        for(div=rems;div>0;div/=10)
+        {
+            digit = div%10;
+            bin=bin*10 + digit;
+        }
+    }
+    public void display()
+    {
+        System.out.println("Binary is: " + bin);
+    }
+    public static void main(String args[])
+    {
+        DeciToBin ob = new DeciToBin();
+        ob.input();
+        ob.compute();
+        ob.display(); 
+    }
+}
+
